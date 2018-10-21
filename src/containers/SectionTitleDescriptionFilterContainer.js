5d2a0fb506ae4b8e4b428b203ca60d273a313630
Pietro Martini <pmartini.crotti@gmail.com>
2018-10-21 07:58:47 -0400

    update container

--- src/containers/SectionTitleDescriptionFilterContainer.js
+++ src/containers/SectionTitleDescriptionFilterContainer.js
@@ -2,11 +2,6 @@ import {connect} from 'react-redux'
 import SectionTitleDescriptionFilter from '../components/SectionTitleDescriptionFilter'
 import {actions} from '../reducers/filterReducer'
 
-const mapState = state => {
-  console.log(state)
-  return {}
-}
-
 const mapDispatch = dispatch => {
   return {
     setTerm: term => dispatch(actions.setTerm(term))
@@ -14,5 +9,5 @@ const mapDispatch = dispatch => {
 }
 
 export default connect(
-  mapState, mapDispatch
+  null, mapDispatch
 )(SectionTitleDescriptionFilter)
